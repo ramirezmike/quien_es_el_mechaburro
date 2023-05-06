6820355bf4d297e947b876bf93b3913b03bf4590
mramirez <ramirezmike2@gmail.com>
2023-05-06 09:15:58 -0400

    Moved old code and started with simple scene

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,245 @@
-#![allow(clippy::type_complexity, clippy::too_many_arguments)]
-#![windows_subsystem = "windows"]
-
-use bevy::ecs::component::Component;
-use bevy::prelude::*;
-//use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
-
-mod asset_loading;
-mod assets;
-mod audio;
-mod bot;
-mod bullet;
-mod burro;
-mod character_select;
-mod collision;
-mod direction;
-mod follow_text;
-mod game_camera;
-mod game_controller;
-mod game_state;
-mod hit;
-mod ingame_ui;
-mod inspect;
-mod levels;
-mod mecha_picker;
-mod menus;
-mod mesh;
-mod pause;
-mod player;
-mod score_display;
-mod smoke;
-mod splash;
-mod title_screen;
-mod ui;
-mod winner;
+use bevy::{
+    core_pipeline::{
+        bloom::{BloomCompositeMode, BloomSettings},
+        tonemapping::Tonemapping,
+    },
+    prelude::*,
+};
+use std::{
+    collections::hash_map::DefaultHasher,
+    hash::{Hash, Hasher},
+};
 
 fn main() {
     App::new()
-        //.add_plugin(LogDiagnosticsPlugin::default())
-        //.add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .insert_resource(ClearColor(Color::DARK_GRAY))
         .add_plugins(DefaultPlugins)
-        .add_plugin(asset_loading::AssetLoadingPlugin)
-        .add_plugin(assets::AssetsPlugin)
-        .add_plugin(audio::GameAudioPlugin)
-        .add_plugin(bot::BotPlugin)
-        .add_plugin(bullet::BulletPlugin)
-        .add_plugin(burro::BurroPlugin)
-        .add_plugin(character_select::CharacterSelectPlugin)
-        .add_plugin(collision::WorldCollisionPlugin)
-        .add_plugin(follow_text::FollowTextPlugin)
-        .add_plugin(game_camera::GameCameraPlugin)
-        .add_plugin(game_controller::GameControllerPlugin)
-        .add_plugin(game_state::GameStatePlugin)
-        .add_plugin(hit::HitPlugin)
-        .add_plugin(ingame_ui::InGameUIPlugin)
-        .add_plugin(inspect::InspectPlugin)
-        .add_plugin(levels::debug::DebugRoomPlugin)
-        .add_plugin(mecha_picker::MechaPickerPlugin)
-        .add_plugin(menus::options::OptionsMenuPlugin)
-        .add_plugin(mesh::MeshPlugin)
-        .add_plugin(pause::PausePlugin)
-        .add_plugin(player::PlayerPlugin)
-        .add_plugin(score_display::ScoreDisplayPlugin)
-        .add_plugin(smoke::SmokePlugin)
-        .add_plugin(splash::SplashPlugin)
-        .add_plugin(title_screen::TitlePlugin)
-        .add_plugin(ui::text_size::TextSizePlugin)
-        .add_plugin(winner::WinnerPlugin)
-        .add_state(AppState::Initial)
-        .add_system_set(SystemSet::on_enter(AppState::Initial).with_system(bootstrap))
-        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+        .add_startup_system(setup_scene)
+        .add_system(update_bloom_settings)
+        .add_system(bounce_spheres)
         .run();
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
-pub enum AppState {
-    Initial,
-    Pause,
-    Debug,
-    Options,
-    InGame,
-    TitleScreen,
-    CharacterSelect,
-    ModelLoading,
-    MechaPicker,
-    ScoreDisplay,
-    Loading,
-    WinnerDisplay,
-    Splash,
-}
-
-fn bootstrap(
-    mut assets_handler: asset_loading::AssetsHandler,
-    mut game_assets: ResMut<assets::GameAssets>,
-    game_state: ResMut<game_state::GameState>,
+fn setup_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
-    assets_handler.load(AppState::Splash, &mut game_assets, &game_state);
+    commands.spawn((
+        Camera3dBundle {
+            camera: Camera {
+                hdr: true, // 1. HDR is required for bloom
+                ..default()
+            },
+            tonemapping: Tonemapping::TonyMcMapface, // 2. Using a tonemapper that desaturates to white is recommended
+            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        BloomSettings::default(), // 3. Enable bloom for the camera
+    ));
+
+    let material_emissive1 = materials.add(StandardMaterial {
+        emissive: Color::rgb_linear(13.99, 5.32, 2.0), // 4. Put something bright in a dark environment to see the effect
+        ..default()
+    });
+    let material_emissive2 = materials.add(StandardMaterial {
+        emissive: Color::rgb_linear(2.0, 13.99, 5.32),
+        ..default()
+    });
+    let material_emissive3 = materials.add(StandardMaterial {
+        emissive: Color::rgb_linear(5.32, 2.0, 13.99),
+        ..default()
+    });
+    let material_non_emissive = materials.add(StandardMaterial {
+        base_color: Color::GRAY,
+        ..default()
+    });
+
+    let mesh = meshes.add(
+        shape::Icosphere {
+            radius: 0.5,
+            subdivisions: 5,
+        }
+        .try_into()
+        .unwrap(),
+    );
+
+    for x in -10..10 {
+        for z in -10..10 {
+            let mut hasher = DefaultHasher::new();
+            (x, z).hash(&mut hasher);
+            let rand = (hasher.finish() - 2) % 6;
+
+            let material = match rand {
+                0 => material_emissive1.clone(),
+                1 => material_emissive2.clone(),
+                2 => material_emissive3.clone(),
+                3 | 4 | 5 => material_non_emissive.clone(),
+                _ => unreachable!(),
+            };
+
+            commands.spawn((
+                PbrBundle {
+                    mesh: mesh.clone(),
+                    material,
+                    transform: Transform::from_xyz(x as f32 * 2.0, 0.0, z as f32 * 2.0),
+                    ..default()
+                },
+                Bouncing,
+            ));
+        }
+    }
+
+    commands.spawn(
+        TextBundle::from_section(
+            "",
+            TextStyle {
+                font: asset_server.load("fonts/FiraMono-Medium.ttf"),
+                font_size: 18.0,
+                color: Color::BLACK,
+            },
+        )
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            position: UiRect {
+                bottom: Val::Px(10.0),
+                left: Val::Px(10.0),
+                ..default()
+            },
+            ..default()
+        }),
+    );
 }
 
-pub fn cleanup<T: Component>(mut commands: Commands, entities: Query<Entity, With<T>>) {
-    for entity in entities.iter() {
-        commands.entity(entity).despawn_recursive();
+// ------------------------------------------------------------------------------------------------
+
+fn update_bloom_settings(
+    mut camera: Query<(Entity, Option<&mut BloomSettings>), With<Camera>>,
+    mut text: Query<&mut Text>,
+    mut commands: Commands,
+    keycode: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    let bloom_settings = camera.single_mut();
+    let mut text = text.single_mut();
+    let text = &mut text.sections[0].value;
+
+    match bloom_settings {
+        (entity, Some(mut bloom_settings)) => {
+            *text = "BloomSettings (Toggle: Space)\n".to_string();
+            text.push_str(&format!("(Q/A) Intensity: {}\n", bloom_settings.intensity));
+            text.push_str(&format!(
+                "(W/S) Low-frequency boost: {}\n",
+                bloom_settings.low_frequency_boost
+            ));
+            text.push_str(&format!(
+                "(E/D) Low-frequency boost curvature: {}\n",
+                bloom_settings.low_frequency_boost_curvature
+            ));
+            text.push_str(&format!(
+                "(R/F) High-pass frequency: {}\n",
+                bloom_settings.high_pass_frequency
+            ));
+            text.push_str(&format!(
+                "(T/G) Mode: {}\n",
+                match bloom_settings.composite_mode {
+                    BloomCompositeMode::EnergyConserving => "Energy-conserving",
+                    BloomCompositeMode::Additive => "Additive",
+                }
+            ));
+            text.push_str(&format!(
+                "(Y/H) Threshold: {}\n",
+                bloom_settings.prefilter_settings.threshold
+            ));
+            text.push_str(&format!(
+                "(U/J) Threshold softness: {}\n",
+                bloom_settings.prefilter_settings.threshold_softness
+            ));
+
+            if keycode.just_pressed(KeyCode::Space) {
+                commands.entity(entity).remove::<BloomSettings>();
+            }
+
+            let dt = time.delta_seconds();
+
+            if keycode.pressed(KeyCode::A) {
+                bloom_settings.intensity -= dt / 10.0;
+            }
+            if keycode.pressed(KeyCode::Q) {
+                bloom_settings.intensity += dt / 10.0;
+            }
+            bloom_settings.intensity = bloom_settings.intensity.clamp(0.0, 1.0);
+
+            if keycode.pressed(KeyCode::S) {
+                bloom_settings.low_frequency_boost -= dt / 10.0;
+            }
+            if keycode.pressed(KeyCode::W) {
+                bloom_settings.low_frequency_boost += dt / 10.0;
+            }
+            bloom_settings.low_frequency_boost = bloom_settings.low_frequency_boost.clamp(0.0, 1.0);
+
+            if keycode.pressed(KeyCode::D) {
+                bloom_settings.low_frequency_boost_curvature -= dt / 10.0;
+            }
+            if keycode.pressed(KeyCode::E) {
+                bloom_settings.low_frequency_boost_curvature += dt / 10.0;
+            }
+            bloom_settings.low_frequency_boost_curvature =
+                bloom_settings.low_frequency_boost_curvature.clamp(0.0, 1.0);
+
+            if keycode.pressed(KeyCode::F) {
+                bloom_settings.high_pass_frequency -= dt / 10.0;
+            }
+            if keycode.pressed(KeyCode::R) {
+                bloom_settings.high_pass_frequency += dt / 10.0;
+            }
+            bloom_settings.high_pass_frequency = bloom_settings.high_pass_frequency.clamp(0.0, 1.0);
+
+            if keycode.pressed(KeyCode::G) {
+                bloom_settings.composite_mode = BloomCompositeMode::Additive;
+            }
+            if keycode.pressed(KeyCode::T) {
+                bloom_settings.composite_mode = BloomCompositeMode::EnergyConserving;
+            }
+
+            if keycode.pressed(KeyCode::H) {
+                bloom_settings.prefilter_settings.threshold -= dt;
+            }
+            if keycode.pressed(KeyCode::Y) {
+                bloom_settings.prefilter_settings.threshold += dt;
+            }
+            bloom_settings.prefilter_settings.threshold =
+                bloom_settings.prefilter_settings.threshold.max(0.0);
+
+            if keycode.pressed(KeyCode::J) {
+                bloom_settings.prefilter_settings.threshold_softness -= dt / 10.0;
+            }
+            if keycode.pressed(KeyCode::U) {
+                bloom_settings.prefilter_settings.threshold_softness += dt / 10.0;
+            }
+            bloom_settings.prefilter_settings.threshold_softness = bloom_settings
+                .prefilter_settings
+                .threshold_softness
+                .clamp(0.0, 1.0);
+        }
+
+        (entity, None) => {
+            *text = "Bloom: Off (Toggle: Space)".to_string();
+
+            if keycode.just_pressed(KeyCode::Space) {
+                commands.entity(entity).insert(BloomSettings::default());
+            }
+        }
+    }
+}
+
+#[derive(Component)]
+struct Bouncing;
+
+fn bounce_spheres(time: Res<Time>, mut query: Query<&mut Transform, With<Bouncing>>) {
+    for mut transform in query.iter_mut() {
+        transform.translation.y =
+            (transform.translation.x + transform.translation.z + time.elapsed_seconds()).sin();
     }
 }
